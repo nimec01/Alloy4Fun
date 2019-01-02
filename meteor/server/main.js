8ea53d407a1433ed20862146ec4031c209d1ddd7
msramalho <up201403027@fe.up.pt>
2019-01-02 20:39:17 +0000

    importing all methods in server main

--- meteor/server/main.js
+++ meteor/server/main.js
@@ -2,7 +2,11 @@ import {
     Meteor
 } from 'meteor/meteor';
 
-import '../server/methods/validate.js'
+import '../server/methods/validate'
+import '../server/methods/genURL'
+import '../server/methods/getInstance'
+import '../server/methods/getProjection'
+import '../server/methods/shareInstance'
 
 Meteor.startup(() => {
     // code to run on server at startup
