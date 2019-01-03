17eb44773c906deac68b381b0c78da85d77d653f
msramalho <up201403027@fe.up.pt>
2019-01-03 22:23:16 +0000

    fixed 2 bugs

--- meteor/client/templates/alloyEditor/genUrl.js
+++ meteor/client/templates/alloyEditor/genUrl.js
@@ -1,75 +1,77 @@
 import {
-    zeroclipboard, getAnchorWithLink
+    zeroclipboard,
+    getAnchorWithLink
 } from "../../lib/editor/clipboard"
 
 export {
     clickGenUrl
 };
 
+/**
+ * Function to handle click on "Share" button
+ * @param {Event} evt 
+ */
 function clickGenUrl(evt) {
-	console.log(evt);
-    if (evt.toElement.id != "genUrl") {
-        var themeData = {
-            atomSettings: atomSettings,
-            relationSettings: relationSettings,
-            generalSettings: generalSettings,
-            currentFramePosition: currentFramePosition,
-            currentlyProjectedTypes: currentlyProjectedTypes
-        };
-        if (!$("#genUrl > button").is(":disabled")) { //if button is not disabled
-            var modelToShare = textEditor.getValue();
+    var themeData = {
+        atomSettings: atomSettings,
+        relationSettings: relationSettings,
+        generalSettings: generalSettings,
+        currentFramePosition: currentFramePosition,
+        currentlyProjectedTypes: currentlyProjectedTypes
+    };
+    if (!$("#genUrl > button").is(":disabled")) { //if button is not disabled
+        var modelToShare = textEditor.getValue();
 
-            if (id = Router.current().params._id) { //if its loaded through an URL its a derivationOf model
-                //so acontece num link publico
-                //handle SECRETs
-                if ((secrets = Router.current().data().secrets) && containsValidSecret(modelToShare)) {
-                    swal({
-                        title: "This model contains information that cannot be shared!",
-                        text: "Are you sure you want to share it?",
-                        type: "warning",
-                        showCancelButton: true,
-                        confirmButtonColor: "#DD6B55",
-                        confirmButtonText: "Yes, share it!",
-                        closeOnConfirm: true
-                    }, function() {
-                        Meteor.call('genURL', modelToShare, "Original", false, Session.get("last_id"), themeData, handleGenURLEvent);
-                    });
-                } else {
-                    if (secrets.length == 0) {
-                        //se tiver um ou mais valid secret com run check e assert anonimos, pergunta
-                        if (containsValidSecretWithAnonymousCommand(modelToShare)) {
-                            swal({
-                                title: "This model contains an anonymous Command!",
-                                text: "Are you sure you want to share it?",
-                                type: "warning",
-                                showCancelButton: true,
-                                confirmButtonColor: "#DD6B55",
-                                confirmButtonText: "Yes, share it!",
-                                closeOnConfirm: true
-                            }, function() {
-                                Meteor.call('genURL', modelToShare, id, false, Session.get("last_id"), themeData, handleGenURLEvent);
-                            });
-                        } else
-                            Meteor.call('genURL', modelToShare, id, false, Session.get("last_id"), themeData, handleGenURLEvent);
-                    } else
-                        Meteor.call('genURL', modelToShare + secrets, id, true, Session.get("last_id"), themeData, handleGenURLEvent)
-                }
-            } else { // Otherwise this a new model (not based in any other)
-                if (containsValidSecretWithAnonymousCommand(modelToShare)) {
-                    swal({
-                        title: "This model contains an anonymous Command!",
-                        text: "Are you sure you want to share it?",
-                        type: "warning",
-                        showCancelButton: true,
-                        confirmButtonColor: "#DD6B55",
-                        confirmButtonText: "Yes, share it!",
-                        closeOnConfirm: true
-                    }, function() {
-                        Meteor.call('genURL', modelToShare, "Original", false, Session.get("last_id"), themeData, handleGenURLEvent);
-                    });
-                } else
+        if (id = Router.current().params._id) { //if its loaded through an URL its a derivationOf model
+            //so acontece num link publico
+            //handle SECRETs
+            if ((secrets = Router.current().data().secrets) && containsValidSecret(modelToShare)) {
+                swal({
+                    title: "This model contains information that cannot be shared!",
+                    text: "Are you sure you want to share it?",
+                    type: "warning",
+                    showCancelButton: true,
+                    confirmButtonColor: "#DD6B55",
+                    confirmButtonText: "Yes, share it!",
+                    closeOnConfirm: true
+                }, function() {
                     Meteor.call('genURL', modelToShare, "Original", false, Session.get("last_id"), themeData, handleGenURLEvent);
+                });
+            } else {
+                if (secrets.length == 0) {
+                    //se tiver um ou mais valid secret com run check e assert anonimos, pergunta
+                    if (containsValidSecretWithAnonymousCommand(modelToShare)) {
+                        swal({
+                            title: "This model contains an anonymous Command!",
+                            text: "Are you sure you want to share it?",
+                            type: "warning",
+                            showCancelButton: true,
+                            confirmButtonColor: "#DD6B55",
+                            confirmButtonText: "Yes, share it!",
+                            closeOnConfirm: true
+                        }, function() {
+                            Meteor.call('genURL', modelToShare, id, false, Session.get("last_id"), themeData, handleGenURLEvent);
+                        });
+                    } else
+                        Meteor.call('genURL', modelToShare, id, false, Session.get("last_id"), themeData, handleGenURLEvent);
+                } else
+                    Meteor.call('genURL', modelToShare + secrets, id, true, Session.get("last_id"), themeData, handleGenURLEvent)
             }
+        } else { // Otherwise this a new model (not based in any other)
+            if (containsValidSecretWithAnonymousCommand(modelToShare)) {
+                swal({
+                    title: "This model contains an anonymous Command!",
+                    text: "Are you sure you want to share it?",
+                    type: "warning",
+                    showCancelButton: true,
+                    confirmButtonColor: "#DD6B55",
+                    confirmButtonText: "Yes, share it!",
+                    closeOnConfirm: true
+                }, function() {
+                    Meteor.call('genURL', modelToShare, "Original", false, Session.get("last_id"), themeData, handleGenURLEvent);
+                });
+            } else
+                Meteor.call('genURL', modelToShare, "Original", false, Session.get("last_id"), themeData, handleGenURLEvent);
         }
     }
 }
@@ -96,14 +98,13 @@ function handleGenURLEvent(err, result) {
     if (!err) {
         // if the URL was generated successfully, create and append a new element to the HTML containing it.
         var url = getAnchorWithLink(result['public'], "public link");
-		var urlPrivate = getAnchorWithLink(result['private'], "private link");
-		console.log(url, urlPrivate);
+        var urlPrivate = getAnchorWithLink(result['private'], "private link");
 
         var textcenter = document.createElement('div');
         textcenter.className = "text-center";
         textcenter.id = "permalink";
-		textcenter.appendChild(url);
-		textcenter.appendChild(urlPrivate);
+        textcenter.appendChild(url);
+        if (urlPrivate) textcenter.appendChild(urlPrivate);
 
         document.getElementById('url-permalink').appendChild(textcenter);
         $("#genUrl > button").prop('disabled', true);
