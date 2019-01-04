635ed380a24e9bd7cb02cd0dacc71b5a37f53e8e
msramalho <up201403027@fe.up.pt>
2019-01-04 19:01:03 +0000

    improved documentation

--- meteor/server/methods/shareInstance.js
+++ meteor/server/methods/shareInstance.js
@@ -1,21 +1,27 @@
 /**
  * Meteor method to store a model instance with the user-defined theme
  * Used in Share Instance option
- * Creates an instance that points to the run/execution
- * Saves the theme and the instance itself
- * @param runID id of the execution
- * @param themeData information about the theme used
- * @param instance object with information about the frame
- * @return url to make possible share the instance
+ * Creates an instance that points to the model
+ * Saves the theme in the instance itself
  */
 Meteor.methods({
-    storeInstance: function(runID, themeData, instance) {
-        var instanceID = Instance.insert({
-            run_id: runID,
+    /**
+     * Saves the instance and returns the id
+     * @param {String} modelId the model id
+     * @param {Bool} sat true or fals for satisfiable instance or not
+     * @param {String} command the name of the command that was executed
+     * @param {String} instance the JSON string of the cytoscape graph
+     * @param {Object} themeData with the theme information for cytoscape
+     * @return id of the new instance
+     */
+    storeInstance: function(modelId, sat, command, instance, themeData) {
+        return Instance.insert({
+            model_id: modelId,
+            sat: sat,
+            command: command,
             graph: instance,
             theme: themeData,
             date: new Date().toLocaleString()
         });
-        return instanceID;
     }
 });
