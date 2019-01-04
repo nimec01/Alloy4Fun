5ccb5e6ad6de8f5a7bc4fd378abb6cb04747b06a
msramalho <up201403027@fe.up.pt>
2019-01-04 21:56:12 +0000

    code cleanup for getInstances

--- meteor/server/methods/getInstances.js
+++ meteor/server/methods/getInstances.js
@@ -1,8 +1,3 @@
-import {
-    Model
-} from '../../lib/collections/model'
-
-
 /**
  * Meteor method to get a model instance
  * This will call the API (webService)
@@ -15,9 +10,9 @@ import {
  * @param last_id
  * @returns Object with the instance data
  */
-
 Meteor.methods({
-    getInstances: function (model, sessionId, numberOfInstances, commandLabel, forceInterpretation, cid, last_id) {
+    //TODO: Daniel, é mesmo suposto manter o forceInterpretation?
+    getInstances: function(model, numberOfInstances, commandLabel, forceInterpretation) {
         return new Promise((resolve, reject) => {
             HTTP.call('POST', `${Meteor.settings.env.API_URL}/getInstances`, {
                 data: {
@@ -28,20 +23,16 @@ Meteor.methods({
                 }
             }, (error, result) => {
                 if (error) reject(error)
-                console.log(result);
                 let content = JSON.parse(result.content)
-                if(content.unsat == true){
+                if (content.unsat == true) {
                     content.commandType = "check";
                     resolve(content);
-                    return;
-                }else{
+                } else {
                     Object.keys(content).forEach(k => {
-                    content[k].commandType = "check";
-                });
-                // console.log(result.content);
-                resolve(content); 
+                        content[k].commandType = "check";
+                    });
+                    resolve(content);
                 }
-               
             });
         })
     }
