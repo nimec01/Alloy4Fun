b31234c7fc6c10824d4c2971a8998b5058904409
msramalho <up201403027@fe.up.pt>
2019-01-04 15:00:06 +0000

    started getModel with complete information

---
+++ meteor/server/methods/getModel.js
@@ -0,0 +1,20 @@
+import {
+    Meteor
+} from "meteor/meteor";
+
+/**
+ * Receives a link (private or public) and returns the corresponding Model
+ * with or without the SECRET code
+ */
+Meteor.methods({
+    getModel: function(linkId) {
+		let link = Link.findOne(linkId)
+		// console.log("getmodel link:", link);
+		let model = Model.findOne(link.model_id)
+		// console.log(model);
+		if(link.isPrivate){
+			console.log("private link");
+		}
+		return model
+    }
+})
