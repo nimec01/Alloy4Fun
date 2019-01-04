2c5e01dd3df281e741c62c41dd733fc66f65218a
msramalho <up201403027@fe.up.pt>
2019-01-04 15:02:04 +0000

    publication started refactoring

--- meteor/server/publications/alloyEditorLoadPublication.js
+++ meteor/server/publications/alloyEditorLoadPublication.js
@@ -9,38 +9,35 @@ import {
 import {
     Run
 } from "../../lib/collections/run"
-import {
-    Link
-} from "../../lib/collections/link"
 
-Meteor.publish('editorLoad', function( /*_id*/ ) { //sem parametro para se conseguir aceder
+// Meteor.publish('editorLoad', function( /*_id*/ ) { //sem parametro para se conseguir aceder
 
-    // TODO está a publicar todos os para o cliente, só deve publicar os requeridos
-    // TODO deve extrair os segredos nesta fase, i.e., os segredos não devem ficar expostos ao cliente. Há que retirar antes de enviar.
+//     // TODO está a publicar todos os para o cliente, só deve publicar os requeridos
+//     // TODO deve extrair os segredos nesta fase, i.e., os segredos não devem ficar expostos ao cliente. Há que retirar antes de enviar.
 
-    //chavetas aninhadas??????
+//     //chavetas aninhadas??????
 
-    var selector = {
-        //_id: _id
-    };
+//     var selector = {
+//         //_id: _id
+//     };
 
-    var options = {
-        fields: {
-            _id: 1,
-            whole: 1,
-            derivationOf: 1
-        }
-    }
+//     var options = {
+//         fields: {
+//             _id: 1,
+//             whole: 1,
+//             derivationOf: 1
+//         }
+//     }
 
-    //empty selector access all Models (required to get the specific Model with the ModelId)
-    var result = Model.find(selector, options);
-    //var result = Model.find(selector, options);
+//     //empty selector access all Models (required to get the specific Model with the ModelId)
+//     var result = Model.find(selector, options);
+//     //var result = Model.find(selector, options);
 
-    if (result) {
-        return result;
-    }
-    return this.ready();
-});
+//     if (result) {
+//         return result;
+//     }
+//     return this.ready();
+// });
 
 Meteor.publish('instanceLoad', function(_id) {
 
@@ -80,30 +77,6 @@ Meteor.publish('runLoad', function() {
 
 
     var result = Run.find(selector, options);
-
-    if (result) {
-        return result;
-    }
-    return this.ready();
-});
-
-Meteor.publish('links', function(_id) {
-
-    //ID do challenge
-    // Selector: {_id : XXXXXX}
-    var selector = {
-        _id: _id
-    };
-
-    var options = {
-        fields: {
-            model_id: 1,
-            private: 1
-        }
-    };
-
-    //puts query return in result variable and is captured in "editLoadController.js"
-    var result = Link.find(selector, options);
 
     if (result) {
         return result;
