7bf4c03f621c7e7c3cb13cf52ac5310af45c63a6
msramalho <up201403027@fe.up.pt>
2019-01-04 15:02:41 +0000

    publication of link started refactoring

--- meteor/server/publications/link.js
+++ meteor/server/publications/link.js
@@ -2,9 +2,16 @@
  * Publishes the link information to the client
  */
 Meteor.publish("link", function(linkId) {
-    return Link.find({
-        _id: linkId
-    }, {
-        fields: Link.publicFields
+    let publication = this;
+    Meteor.call("getModel", linkId, (err, model) => {
+        if(err) publication.ready();
+        publication.added('Model', linkId, model);
+        publication.ready();
     })
+
+    // return Link.find({
+    //     _id: linkId
+    // }, {
+    //     fields: Link.publicFields
+    // })
 });
