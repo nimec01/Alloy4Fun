5e87ce786bbe69ff6a31d9450d54ab49e0e7fecc
msramalho <up201403027@fe.up.pt>
2019-01-05 02:18:15 +0000

    return private info and apply retrieve commands from code

--- meteor/server/methods/getModel.js
+++ meteor/server/methods/getModel.js
@@ -4,6 +4,7 @@ import {
 import {
     extractSecrets
 } from "../lib/secrets"
+import {getCommandsFromCode} from "../../lib/editor/text";
 /**
  * Receives a link (private or public) and returns the corresponding Model
  * with or without the SECRET code
@@ -11,9 +12,11 @@ import {
 Meteor.methods({
     getModel: function(linkId) {
         let link = Link.findOne(linkId)
-		let model = Model.findOne(link.model_id)
-		if (!link.private) model.whole = extractSecrets(model.whole).public
-		model.model_id = model._id // this is necessary because publish is for the linkId
+        let model = Model.findOne(link.model_id)
+        if (!link.private) model.whole = extractSecrets(model.whole).public
+        model.model_id = model._id // this is necessary because publish is for the linkId
+        model.from_private = link.private // return info about the used link type
+        model.commands = getCommandsFromCode(model.whole)
         return model
     },
     getInstance: function(linkId) {
