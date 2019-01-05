39104605743b7c7048b60f43bf2349dda5b2f9d4
msramalho <up201403027@fe.up.pt>
2019-01-05 15:35:30 +0000

    retrieve model from link or instance

--- meteor/server/methods/getModel.js
+++ meteor/server/methods/getModel.js
@@ -4,23 +4,52 @@ import {
 import {
     extractSecrets
 } from "../lib/secrets"
-import {getCommandsFromCode} from "../../lib/editor/text";
+import {
+    getCommandsFromCode
+} from "../../lib/editor/text";
 /**
  * Receives a link (private or public) and returns the corresponding Model
  * with or without the SECRET code
  */
 Meteor.methods({
-    getModel: function(linkId) {
-        let link = Link.findOne(linkId)
-        let model = Model.findOne(link.model_id)
-        let complete_model = model.code
-        if (!link.private) model.code = extractSecrets(model.code).public
-        model.model_id = model._id // this is necessary because publish is for the linkId
-        model.from_private = link.private // return info about the used link type
-        model.commands = getCommandsFromCode(complete_model)
-        return model
-    },
-    getInstance: function(linkId) {
-        //TODO: 
+    /**
+     * retrieves the Link -> the Model -> extracts Secrets if necessary ->
+     * extracts secret commands -> returns the model
+     * @param {String} id
+     */
+    getModel: function(id) {
+        return getModelFromLink(id) || getModelFromInstance(id)
     }
 })
+
+/**
+ * retrieves the Link -> the Model -> extracts Secrets if necessary ->
+ * extracts secret commands -> returns the model
+ * @param {String} id
+ */
+function getModelFromLink(linkId) {
+    let link = Link.findOne(linkId)
+    if (!link) return //undefined if link does not exist
+    let model = Model.findOne(link.model_id)
+    let complete_model = model.code
+    if (!link.private) model.code = extractSecrets(model.code).public
+    model.from_private = link.private // return info about the used link type
+    model.commands = getCommandsFromCode(complete_model)
+    model.model_id = model._id // this is necessary because publish is for the linkId
+    return model
+}
+
+/**
+ * Does not have to clean SECRETS as share instance sends the code as-is
+ * @param {String} instanceId the potential instance id
+ */
+function getModelFromInstance(instanceId) {
+    let instance = Instance.findOne(instanceId)
+    if (!instance) return //undefined if instance does not exist
+    let model = Model.findOne(instance.model_id)
+    model.instance = instance // so that frontend can access the instance
+    model.from_private = false // so as to load the secrets from original on execute
+    model.commands = getCommandsFromCode(model.code)
+    model.model_id = model._id // this is necessary because publish is for the linkId
+    return model
+}
