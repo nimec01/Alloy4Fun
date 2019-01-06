f1f65bcf24c4fdc7bc2922e04f1b7152d988e857
msramalho <up201403027@fe.up.pt>
2019-01-06 15:17:28 +0000

    organized docs

--- meteor/server/publications/modelFromLink.js
+++ meteor/server/publications/modelFromLink.js
@@ -4,8 +4,8 @@
 Meteor.publish("modelFromLink", function(linkId) {
     let publication = this;
     Meteor.call("getModel", linkId, (err, model) => {
-        if (!err) publication.added('Model', linkId, model);
         // see https://docs.meteor.com/api/pubsub.html#Subscription-added
+        if (!err) publication.added('Model', linkId, model);
         publication.ready();
     })
 })
