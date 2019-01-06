5d1e900f570dde19c1a1d994837a1a9a3954df9f
msramalho <up201403027@fe.up.pt>
2019-01-06 15:17:06 +0000

    simplified code

--- meteor/server/publications/modelFromLink.js
+++ meteor/server/publications/modelFromLink.js
@@ -4,9 +4,8 @@
 Meteor.publish("modelFromLink", function(linkId) {
     let publication = this;
     Meteor.call("getModel", linkId, (err, model) => {
-        if(err) publication.ready();
+        if (!err) publication.added('Model', linkId, model);
         // see https://docs.meteor.com/api/pubsub.html#Subscription-added
-        publication.added('Model', linkId, model);
         publication.ready();
     })
 })
