0af0569cfe3316d1fd779c40ad7bbf8f796c952b
msramalho <up201403027@fe.up.pt>
2019-01-06 02:30:26 +0000

    test improvements

--- meteor/client/templates/alloyEditor/genUrl.test.js
+++ meteor/client/templates/alloyEditor/genUrl.test.js
@@ -1,13 +1,10 @@
 import {
-    chai,
-    assert
+    chai
 } from 'meteor/practicalmeteor:chai';
 import {
     containsValidSecretWithAnonymousCommand
 } from "./genUrl"
-/**
- * Default meteor tests for programming principles to be forced
- */
+
 describe("gen url submethods work", function() {
     it("identifies anonymous methods", function() {
         chai.assert.isFalse(containsValidSecretWithAnonymousCommand("//SECRET\ncheck test{}"))
