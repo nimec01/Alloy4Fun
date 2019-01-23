e522094d18b71f603fcda57eadb1fa0faa6b73a0
nmacedo <nfmmacedo@di.uminho.pt>
2019-01-23 13:26:40 +0000

    updated lib tests

--- meteor/lib/editor/text.test.js
+++ meteor/lib/editor/text.test.js
@@ -5,26 +5,23 @@ import {
 import {
     isParagraph,
     containsValidSecret,
-    getCommandsFromCode
+    getCommandsFromCode,
+    secretTag
 } from "./text"
 /**
  * Default meteor tests for programming principles to be forced
  */
 describe("editor text util functions", function() {
-    it("identifies invalid paragraphs", function() {
-        chai.assert.isFalse(isParagraph("one sig"))
-        chai.assert.isFalse(isParagraph("lol one sig A"))
-    });
-    it("identifies valid paragraphs", function() {
-        chai.assert.isTrue(isParagraph("one sig A"))
-        chai.assert.isTrue(isParagraph("pred X {}"))
-    });
     it("identifies invalid secrets", function() {
+        chai.assert.isFalse(containsValidSecret("/*\n//SECRET\n*/\nsig a {}"))
+        chai.assert.isFalse(containsValidSecret("//SECRET  \nsig a {}"))
+        chai.assert.isFalse(containsValidSecret(" //SECRET\nsig a {}"))
         chai.assert.isFalse(containsValidSecret("something"))
         chai.assert.isFalse(containsValidSecret("something/SECRET"))
         chai.assert.isFalse(containsValidSecret("something//SECRET\n"))
     });
     it("identifies valid secrets", function() {
+        chai.assert.isTrue(containsValidSecret("//SECRET\nsig a {}"))
         chai.assert.isTrue(containsValidSecret("something\n//SECRET\nthis is the secret"))
         chai.assert.isTrue(containsValidSecret("\n//SECRET\nthis is the secret"))
     });
@@ -38,6 +35,6 @@ run run3 {}
 check check1 for 5
 check check2
 `
-        chai.assert.sameMembers(getCommandsFromCode(code), ["run1", "run2", "run3", "check1", "check2"])
+        chai.assert.sameMembers(getCommandsFromCode(code), ["run run1", "run run2", "run run3", "check check1", "check check2"])
     });
 });
