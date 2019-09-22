ce474268c577ea196400f337447394bb2217fb44
nmacedo <nfmmacedo@di.uminho.pt>
2019-09-22 22:09:07 +0100

    removed side panel options

--- meteor/client/templates/visSettings/visSettings.js
+++ meteor/client/templates/visSettings/visSettings.js
@@ -7,15 +7,3 @@ Template.visSettings.helpers({
     }
 })
 
-Template.visSettings.events({
-    'click #settings-button'() {
-        $('.settings-panel').toggleClass('open')
-    }
-})
-
-Template.visSettings.onRendered(() => {
-    // Add styling to scroll bar on theme settings
-    $('.scroll-settings').slimScroll({
-        height: '380px'
-    })
-})
