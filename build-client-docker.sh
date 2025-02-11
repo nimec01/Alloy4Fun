2845ec63d4152cf92e8d434fc88617e9da52189d
nimec01 <24428341+nimec01@users.noreply.github.com>
2025-02-11 13:29:23 +0100

    add build script for meteor client

---
+++ build-client-docker.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env zsh
+
+set -eux
+
+CACHE=../.cache/client
+CACHE_TO=--cache-to=type=local,dest=${CACHE}/dist,mode=max
+
+mkdir -p ${CACHE}/base/ ${CACHE}/dist/
+
+# Create a new builder instance
+
+echo "Creating a new builder instance..."
+
+docker buildx create --use --name=alloy4fun-builder --driver docker-container --node larger_log --driver-opt env.BUILDKIT_STEP_LOG_MAX_SIZE=500000000
+
+# Build the client
+
+echo "Building the client..."
+
+cd meteor
+
+docker buildx build --load --cache-from=type=local,src=${CACHE}/dist ${CACHE_TO} --tag alloy4fun-meteor:fmi --file Dockerfile .
+
+cd -
+
+# Copy the build folder
+
+docker create --name alloy4fun-meteor alloy4fun-meteor:fmi
+
+docker cp alloy4fun-meteor:/opt/meteor/dist/bundle/ meteor-dist/bundle/
+
+docker rm alloy4fun-meteor
