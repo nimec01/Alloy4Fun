4e6e32f9e5441a96eb33648a0cac542f42940d83
nimec01 <24428341+nimec01@users.noreply.github.com>
2025-02-11 15:58:16 +0100

    start publish script

---
+++ publish.sh
@@ -0,0 +1,18 @@
+
+# Pack api
+
+TEMP_API_DIR=$(mktemp -d)
+
+cp alloy4fun-api.jar $TEMP_API_DIR
+
+echo "/usr/bin/java -Djava.net.preferIPv4Stack=true -jar alloy4fun-api.jar" > $TEMP_API_DIR/start.sh
+
+chmod +x $TEMP_API_DIR/start.sh
+
+mkdir -p $TEMP_API_DIR/config
+
+cp config/api.keter.yaml $TEMP_API_DIR/config/keter.yaml
+
+tar -C $TEMP_API_DIR -czf alloy4fun-api.keter .
+
+rm -rf $TEMP_API_DIR
