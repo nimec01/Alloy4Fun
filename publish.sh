572286fa20597f58679c590f5a47074eb50b3fdd
nimec01 <24428341+nimec01@users.noreply.github.com>
2025-02-11 16:20:20 +0100

    make api adhere to PORT environment variable

--- publish.sh
+++ publish.sh
@@ -5,7 +5,7 @@ TEMP_API_DIR=$(mktemp -d)
 
 cp alloy4fun-api.jar $TEMP_API_DIR
 
-echo "/usr/bin/java -Djava.net.preferIPv4Stack=true -jar alloy4fun-api.jar" > $TEMP_API_DIR/start.sh
+echo "/usr/bin/java -Djava.net.preferIPv4Stack=true -Dthorntail.http.port=\${PORT:=8080} -jar alloy4fun-api.jar" > $TEMP_API_DIR/start.sh
 
 chmod +x $TEMP_API_DIR/start.sh
 
