6c982bc0bf4bd83d82c3e6e294a37a6fc962de0c
nimec01 <24428341+nimec01@users.noreply.github.com>
2025-02-13 16:37:54 +0100

    include deploy section in publish.sh

--- publish.sh
+++ publish.sh
@@ -37,3 +37,52 @@ tar -C $TEMP_METEOR_DIR -czf alloy4fun-meteor.keter .
 
 rm -rf $TEMP_METEOR_DIR meteor-dist/
 
+# Deploy (assumes that java-8 and Node.js v14 are installed on the server and included in PATH)
+
+USER=
+GROUP=${USER}
+SSH_USER=${USER}
+SERVER=
+JUMP_HOST=
+PORT=22
+
+API_KET=alloy4fun-api.keter
+API_TARGET_FOLDER=/opt/alloy4fun-api
+
+METEOR_KET=alloy4fun-meteor.keter
+METEOR_TARGET_FOLDER=/opt/alloy4fun-meteor
+
+LC_ALL=C
+
+TIME=`date +"%Y-%m-%d-%H.%M.%S"`
+if ! [[ -v REMOTE_DIR ]]
+then
+  API_REMOTE_DIR=${API_TARGET_FOLDER}${TIME}
+  METEOR_REMOTE_DIR=${METEOR_TARGET_FOLDER}${TIME}
+fi
+
+ssh ${JUMP_HOST:+-J} ${JUMP_HOST:+"${JUMP_HOST}"}\
+  -p "${PORT}" "${SSH_USER}@${SERVER}" -C "mkdir -p ${API_REMOTE_DIR} ${METEOR_REMOTE_DIR}"
+
+scp ${JUMP_HOST:+-J} ${JUMP_HOST:+"${JUMP_HOST}"}\
+  -P "${PORT}" "$API_KET" "${SSH_USER}@${SERVER}:${API_REMOTE_DIR}/$API_KET"
+
+scp ${JUMP_HOST:+-J} ${JUMP_HOST:+"${JUMP_HOST}"}\
+  -P "${PORT}" "$METEOR_KET" "${SSH_USER}@${SERVER}:${METEOR_REMOTE_DIR}/$METEOR_KET"
+
+ssh ${JUMP_HOST:+-J} ${JUMP_HOST:+"${JUMP_HOST}"}\
+  -p "${PORT}" "${SSH_USER}@${SERVER}" -C "set -x trace\
+  && mkdir ${METEOR_REMOTE_DIR}/meteor\
+  && cd ${METEOR_REMOTE_DIR}/meteor\
+  && tar xf ../${METEOR_KET}\
+  && cd ${METEOR_REMOTE_DIR}/meteor/bundle/programs/server\
+  && npm install --only=production\
+  && chown -R ${USER}:${GROUP} ${METEOR_REMOTE_DIR}\
+  && rm -rf ${METEOR_REMOTE_DIR}/${METEOR_KET} ${METEOR_TARGET_FOLDER}\
+  && ln -sf ${METEOR_REMOTE_DIR}/meteor ${METEOR_TARGET_FOLDER}\
+  && tar czf ${METEOR_REMOTE_DIR}/${METEOR_KET} -C ${METEOR_REMOTE_DIR}/meteor config/ start.sh\
+  && rm -rf ${METEOR_REMOTE_DIR}/meteor/config ${METEOR_REMOTE_DIR}/meteor/start.sh\
+  && chown -R ${USER}:${GROUP} ${METEOR_REMOTE_DIR}/${METEOR_KET}\
+  && mv ${METEOR_REMOTE_DIR}/${METEOR_KET} /opt/keter/incoming/\
+  && mv ${API_REMOTE_DIR}/${API_KET} /opt/keter/incoming/\
+  && service keter restart"
